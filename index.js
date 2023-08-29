0af04574b15800b6c39d530e0212946678f18d3c
kzh4295 <kzh4295@ncits.co.kr>
2023-08-29 19:58:30 +0900

    Design : 세팅 및 기본 ui 구성

---
+++ index.js
@@ -0,0 +1,32 @@
+const taskInput = document.getElementById("myInput");
+const taskList = document.getElementById("taskList");
+
+taskInput.addEventListener('keydown', function(event) {
+  const taskText = taskInput.value.trim();
+  if (event.key === 'Enter' && taskText !== "") {
+    const taskItem = createTaskItem(taskText);
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+  }
+});
+
+function createTaskItem(text) {
+  console.log(text)
+  const taskItem = document.createElement("li");
+  const taskText = document.createElement("span");
+  const deleteButton = document.createElement("img");
+
+  taskText.textContent = text;
+  deleteButton.src = "delete.png";
+
+  deleteButton.addEventListener("click", () => {
+    taskList.removeChild(taskItem);
+  });
+
+  taskItem.appendChild(taskText).classList.add('add');
+  taskItem.appendChild(deleteButton).classList.add('delete-btn');
+
+  return taskItem;
+}
+
+
