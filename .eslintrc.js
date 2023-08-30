c284d4efa1b236690c5118b9733de120eea0d3e5
kzh4295 <kzh4295@ncits.co.kr>
2023-08-30 14:55:55 +0900

    chore: 8/29(화) 코드 리뷰 이후 코드 수정

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'html'],
+  rules: {
+    indent: ['error', 2],
+    // 세미콜론 사용 여부 (세미콜론을 사용하도록 설정)
+    semi: ['error', 'always'],
+    // 따옴표 사용 여부 (싱글 따옴표 사용하도록 설정)
+    quotes: ['error', 'single'],
+    // 함수 선언 시 공백 규칙 (function 키워드 뒤에 공백 적용)
+    // 'space-before-function-paren': ['error', 'always'],
+  },
+};
--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-const taskInput = document.getElementById('myInput');
-const taskList = document.getElementById('taskList');
+const $taskInput = document.getElementById('taskInput');
+const $taskList = document.getElementById('taskList');
 
-taskInput.addEventListener('keydown', function (event) {
-  const taskText = taskInput.value.trim();
-  if (event.key === 'Enter' && taskText !== '') {
-    const taskItem = createTaskItem(taskText);
-    taskList.appendChild(taskItem);
-    taskInput.value = '';
+const handleKeydownTaskInput = (event) => {
+  const taskText = $taskInput.value.trim();
+
+  if (event.key !== 'Enter' || taskText === '') {
+    return;
   }
-});
 
-function createTaskItem(text) {
-  console.log(text);
-  const taskItem = document.createElement('li');
-  const taskText = document.createElement('span');
-  const deleteButton = document.createElement('img');
+  const taskItem = createTaskItemElement(taskText);
+  $taskList.appendChild(taskItem);
+  $taskInput.value = '';
+};
 
-  taskText.textContent = text;
-  deleteButton.src = 'delete.png';
+$taskInput.addEventListener('keydown', handleKeydownTaskInput);
 
-  deleteButton.addEventListener('click', () => {
-    taskList.removeChild(taskItem);
+const createTaskItemElement = (text) => {
+  const taskItemLi = document.createElement('li');
+  const taskTextSpan = document.createElement('span');
+  const deleteButtonImg = document.createElement('img');
+  var svgImage =
+    '<svg fill="#af5b5e" width="20px" height="20px" viewBox="-3.5 0 19 19" xmlns="http://www.w3.org/2000/svg" class="cf-icon-svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M11.383 13.644A1.03 1.03 0 0 1 9.928 15.1L6 11.172 2.072 15.1a1.03 1.03 0 1 1-1.455-1.456l3.928-3.928L.617 5.79a1.03 1.03 0 1 1 1.455-1.456L6 8.261l3.928-3.928a1.03 1.03 0 0 1 1.455 1.456L7.455 9.716z"></path></g></svg>';
+
+  taskTextSpan.textContent = text;
+  deleteButtonImg.src = 'data:image/svg+xml,' + encodeURIComponent(svgImage);
+
+  deleteButtonImg.addEventListener('click', () => {
+    $taskList.removeChild(taskItemLi);
   });
 
-  taskItem.appendChild(taskText).classList.add('add');
-  taskItem.appendChild(deleteButton).classList.add('delete-btn');
+  taskItemLi.appendChild(taskTextSpan).classList.add('add');
+  taskItemLi.appendChild(deleteButtonImg).classList.add('delete-btn');
 
-  return taskItem;
-}
+  return taskItemLi;
+};
