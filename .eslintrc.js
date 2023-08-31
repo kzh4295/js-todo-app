cd8f23feba041fad2bc1f3a71528ac70b1a485dc
kzh4295 <kzh4295@ncits.co.kr>
2023-08-31 15:19:18 +0900

    chore 회사에서 작업

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   overrides: [
     {
       env: {
@@ -19,7 +15,6 @@ module.exports = {
       },
     },
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -27,11 +22,7 @@ module.exports = {
   plugins: ['html'],
   rules: {
     indent: ['error', 2],
-    // 세미콜론 사용 여부 (세미콜론을 사용하도록 설정)
     semi: ['error', 'always'],
-    // 따옴표 사용 여부 (싱글 따옴표 사용하도록 설정)
     quotes: ['error', 'single'],
-    // 함수 선언 시 공백 규칙 (function 키워드 뒤에 공백 적용)
-    // 'space-before-function-paren': ['error', 'always'],
   },
 };
--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -1,14 +1,7 @@
 ## 요약
-스타일링 변경
 
 ## 관련 이슈
 
-
 ## 변경 사항
-- 스타일링 변경
-- 버그 수정
-- Pull Request Template 만들기
-- prettier, eslint 세팅하기
 
 ## 참고 사항 
-
