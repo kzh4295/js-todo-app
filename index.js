6a9c5a7a68d669b9b07022bb029f99a5b4dfb4b6
kzh4295 <kzh4295@ncits.co.kr>
2023-09-04 14:59:38 +0900

    refactoring:모듈 분리 및 코드 정리

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
-// utils
-function $(selector) {
-  return document.getElementById(selector);
-}
+import { initializeTaskInput } from './taskInput.js';
+import { createTaskItemElement } from './taskItem.js';
+import { $ } from './utilFunc.js';
 
-const $taskInput = $('#taskInput');
-const $taskList = $('#taskList');
+const $taskInput = $('taskInput');
+const $taskList = $('taskList');
 
-const handleKeydownTaskInput = (event) => {
-  const taskText = $taskInput.value.trim();
-
-  if (event.key !== 'Enter' || !taskText) {
-    return;
-  }
-
-  const taskItem = createTaskItemElement(taskText);
-  $taskList.appendChild(taskItem);
-  $taskInput.value = '';
-};
-
-$taskInput.addEventListener('keydown', handleKeydownTaskInput);
-
-const createTaskItemElement = (text) => {
-  const taskItemLi = document.createElement('li');
-  const taskTextSpan = document.createElement('span');
-  const deleteButtonImg = document.createElement('img');
-  const svgImage =
-    '<svg fill="#af5b5e" width="40px" height="40px" viewBox="-3.5 0 19 19" xmlns="http://www.w3.org/2000/svg" class="cf-icon-svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M11.383 13.644A1.03 1.03 0 0 1 9.928 15.1L6 11.172 2.072 15.1a1.03 1.03 0 1 1-1.455-1.456l3.928-3.928L.617 5.79a1.03 1.03 0 1 1 1.455-1.456L6 8.261l3.928-3.928a1.03 1.03 0 0 1 1.455 1.456L7.455 9.716z"></path></g></svg>';
-
-  taskTextSpan.textContent = text;
-  deleteButtonImg.src = 'data:image/svg+xml,' + encodeURIComponent(svgImage);
-
-  const handleClickDeleteButtonImg = () => {
-    $taskList.removeChild(taskItemLi);
-  };
-
-  deleteButtonImg.addEventListener('click', handleClickDeleteButtonImg);
-
-  taskItemLi.appendChild(taskTextSpan).classList.add('add');
-  taskItemLi.appendChild(deleteButtonImg).classList.add('deleteBtn');
-
-  return taskItemLi;
-};
+initializeTaskInput($taskInput, $taskList, createTaskItemElement);
